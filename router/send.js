613f52b02017c5359ddbd1346af21d5145bc8100
kleinercubs <1910481653@qq.com>
2020-09-07 22:14:42 +0800

    Add database to proccess verification.

--- router/send.js
+++ router/send.js
@@ -1,55 +1,79 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser')
-var nodemailer = require('nodemailer')
+var mailer = require('./mailer.js')
+var moment = require('moment');
+// var connection = require('./database.js');
+var SqliteDB = require('./database.js').SqliteDB
 
-var transporter = nodemailer.createTransport({
-  host: "host",// 发送方邮箱
-  port: 465, //端口号
-  secure: true, // true for 465, false for other ports
-  auth: {
-      user: 'name', // 发送方的邮箱地址 
-      pass: 'password'// smtp验证码 
-  }
-});
+var db  = new SqliteDB('acm.db')
+db.createTable()
+
+var createTableSql = "CREATE TABLE IF NOT EXISTS CAPTCHA(EMAIL STRING, CAPTCHACODE STRING, REQ_TIME DATETIME, AVAILABLE INTEGER)"
+var sql_get_existed_captcha = "SELECT CAPTCHACODE FROM CAPTCHA WHERE EMAIL = ?"
+var sql_get_available_captcha = "SELECT CAPTCHACODE FROM CAPTCHA WHERE EMAIL = ? AND TIMESTAMPDIFF(MINUTE, REQ_TIME, ?) <= 5 AND AVAILABLE"
+var sql_update_captcha = "UPDATE CAPTCHA SET CAPTCHACODE = ?, REQ_TIME = ?, AVAILABLE = ? WHERE EMAIL = ?"
+var sql_insert_captcha = "INSERT INTO CAPTCHA (EMAIL, CAPTCHACODE, REQ_TIME, AVAILABLE) VALUES(?, ?, ?, ?)"
+var sql_delete_captcha = "DELETE FROM CAPTCHA WHERE EMAIL = ?"
 
 router.use((req, res, next) => {
-//  console.log('Success Time: ', Date.now())
   next()
 })
 
-router.post('/', (req, res, next) => {
-  var info = (req.body.data.experience === undefined) ? 'none' : `${req.body.data.experience}`
-  var content = `${req.body.data.username}你好:\n` + 
-                `感谢你报名复旦大学程序设计竞赛队！这是一封信息确认邮件，如没有信息错误请不要回复。\n` + 
-                `我们将尽快通过邮件、微信等方式与你取得联系。\n` + 
-                `学号: ${req.body.data.studentid}\n` + 
-                `性别: ${req.body.data.gender}\n` +
-                `毕业高中: ${req.body.data.highschool}\n` + 
-                `专业: ${req.body.data.major}\n` +
-                `竞赛或相关经历: ${info}\n` + 
-                `个人介绍: ${req.body.data.introduction}\n` + 
-                `手机号: ${req.body.data.cellphone}\n` + 
-                `微信: ${req.body.data.wechat}\n`
-  
-  var mail = {
-    from: 'addr',
-    to: `${req.body.data.email}`,
-    bcc: `bcc_addr`,
-    subject: '【信息确认】复旦大学程序设计竞赛队申请',
-    text: content
-  }
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        msg: 'fail'
-      })
-    } else {
-      res.json({
-        msg: 'success'
-      })
+router.post('/captcha', (req, res, next) => {
+  console.log(req.body.data)
+  var receiver = req.body.data.email
+  var captchaCode = ""
+  for (var i = 0; i < 6; i++)
+    captchaCode += Math.floor(Math.random() * 10)
+  var req_time = moment().format("YYYY-MM-DD HH:mm:ss")
+
+  console.log(receiver, captchaCode, req_time)
+  db.queryExistedData({email: receiver, captchaCode: captchaCode, req_time: req_time, available: 1}, solution)
+  function solution(stat) {
+    var content = `同学你好:\n` + 
+                  `你的验证码为: ${captchaCode}\n` 
+    console.log(stat)
+    if (stat.err != null){
+      res.json({msg: 'fail'})
+    } else{
+      mailer.send(receiver, content, `验证码`, res)
+      console.log(content)
     }
-  })
+  }
 })
+
+router.post('/confirm', (req, res, next) => {
+  var receiver = req.body.data.email
+  var captchaCode = req.body.data.captcha
+  var req_time = moment().format("YYYY-MM-DD HH:mm:ss")
+  console.log(receiver, captchaCode, req_time)
+  db.queryAvailableData({email: receiver, captchaCode: captchaCode, req_time: req_time, available: 1}, solution)
+  function solution(stat) {
+    var info = (req.body.data.experience === undefined) ? 'none' : `${req.body.data.experience}`
+    var content = `${req.body.data.username}你好:\n` + 
+                  `感谢你报名复旦大学程序设计竞赛队！这是一封信息确认邮件，如没有信息错误请不要回复。\n` + 
+                  `我们将尽快通过邮件、微信等方式与你取得联系。\n` + 
+                  `学号: ${req.body.data.studentid}\n` + 
+                  `性别: ${req.body.data.gender}\n` +
+                  `毕业高中: ${req.body.data.highschool}\n` + 
+                  `专业: ${req.body.data.major}\n` +
+                  `竞赛或相关经历: ${info}\n` + 
+                  `个人介绍: ${req.body.data.introduction}\n` + 
+                  `手机号: ${req.body.data.cellphone}\n` + 
+                  `微信: ${req.body.data.wechat}\n`
+    console.log(stat)
+    if (stat.err != null){
+      res.json({msg: 'fail'})
+    } else{
+      if (stat.msg == 'captchaError') res.json({msg: stat.msg})
+      else if (stat.msg == 'success'){
+        mailer.send(receiver, content, `信息确认`, res)
+        console.log(content)
+      }
+    }
+  }
+})
+
 
 module.exports = router;
--- src/Join.js
+++ src/Join.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { Form, Input, Select, Button, Typography } from 'antd'
+import { Form, Input, Select, Button, Typography, Col, Row } from 'antd'
 import 'antd/dist/antd.css'
 import './css/index.css'
 
@@ -23,124 +23,62 @@ const tailLayout = {
     },
 };
 
+var mail = "NULL"
 export default class extends Component {
     formRef = React.createRef();
-    constructor(props) {
-        super(props)
-        this.state = {
-            code: "",
-            codeLength: 4,
-            fontSizeMin: 25,
-            fontSizeMax: 30,
-            colorMin: 10,
-            colorMax: 20,
-            lineColorMin: 100,
-            lineColorMax: 180,
-            contentWidth: 90,
-            contentHeight: 30,
-        }
-    }
-   
-    componentWillMount() {
-      this.canvas = React.createRef()
-    }
-   
-    componentDidMount() {
-      this.drawPic()
-    }
-   
-    randomNum = (min, max) => {
-      return Math.floor(Math.random() * (max - min) + min)
-    };
-   
-    randomColor(min, max) {
-      const r = this.randomNum(min, max)
-      const g = this.randomNum(min, max)
-      const b = this.randomNum(min, max)
-      return `rgb(${r}, ${g}, ${b})`
-    }
-   
-    drawText(captcha, txt, i) {
-      captcha.fillStyle = this.randomColor(this.state.colorMin, this.state.colorMax)
-      const fontSize = this.randomNum(this.state.fontSizeMin, this.state.fontSizeMax)
-      captcha.font = `${fontSize}px SimHei`
-      const offset = (this.state.contentWidth - 30) / (this.state.code.length - 1)
-      let x = 10 + i * offset
-      let y = this.randomNum(this.state.fontSizeMax, this.state.contentHeight - 5)
-      const deg = this.randomNum(-10, 10)
-      captcha.translate(x, y)
-      captcha.rotate((deg * Math.PI) / 180)
-      captcha.fillText(txt, 0, 0)
-      captcha.rotate((-deg * Math.PI) / 180)
-      captcha.translate(-x, -y)
-    }
-   
-    drawLine(captcha) {
-      for (let i = 0; i < 5; i++) {
-        captcha.strokeStyle = this.randomColor(this.state.lineColorMin, this.state.lineColorMax)
-        captcha.beginPath();
-        captcha.moveTo(this.randomNum(0, this.state.contentWidth), this.randomNum(0, this.state.contentHeight))
-        captcha.lineTo(this.randomNum(0, this.state.contentWidth), this.randomNum(0, this.state.contentHeight))
-        captcha.stroke()
-      }
-    }
-   
-    drawDot(captcha) {
-      for (let i = 0; i < 100; i++) {
-        captcha.fillStyle = this.randomColor(0, 255)
-        captcha.beginPath();
-        captcha.arc(
-          this.randomNum(0, this.state.contentWidth),
-          this.randomNum(0, this.state.contentHeight),
-          1, 0, 2 * Math.PI
-        );
-        captcha.fill()
-      }
-    }
-   
-    reloadPic = () => {
-      this.drawPic();
-    }
-
-    drawPic() {
-        let random = "";
-        const str = "ABCDEFGHJKLMNPQRSTUVWXYZ";
-        for (let i = 0; i < this.state.codeLength; i++)
-            random += str[Math.floor(Math.random() * 24)]
-        this.setState(
-            {
-                code: random, 
-            },
-            () => {
-                const canvas = this.canvas.current
-                const captcha = canvas.getContext("2d")
-                captcha.textBaseline = "bottom"
-                captcha.fillStyle = `rgb(250, 250, 250)`
-                captcha.fillRect(0, 0, this.state.contentWidth, this.state.contentHeight)
-                for (let i = 0; i < this.state.code.length; i++)
-                    this.drawText(captcha, this.state.code[i], i)
-                this.drawLine(captcha)
-                this.drawDot(captcha)
-            }
-        )
-    }
 
     onReset = () => {
         this.formRef.current.resetFields()
-        this.reloadPic()
     }
 
     onFinish = (values) => {
-        if (values.captchaCode.toLowerCase() === this.state.code.toLowerCase()) {
-            axios.post('/send', { 
-                data : values
+        console.log(values)
+        axios.post('/send/confirm', { 
+            data : values
+        }
+        ).then((response)=>{
+            if (response.data.msg === 'success'){
+                alert('提交成功！\n我们将尽快通过acm@fudan.edu.cn联系您。\n如有任何问题或一周内未收到联系邮件，欢迎邮件联系我们！感谢您的投递:)')
+                this.onReset()
+            } else if (response.data.msg === 'captchaError'){
+                alert('验证码错误。请检查或更新验证码。')
+            } else if (response.data.msg === 'fail'){
+                alert('请重新提交。')
+            }
+        }).catch(function (error) {
+            if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data)
+            console.log(error.response.status)
+            console.log(error.response.headers)
+            } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request)
+            } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message)
+            }
+            console.log(error.config)
+        })
+    }
+
+    SendCaptcha = (values) =>{
+        console.log(mail)
+        if (mail === "NULL"){
+            console.log("email is undefined")
+            alert("请填写邮箱")
+        } else{
+            axios.post('/send/captcha', { 
+                "data" : {"email" : mail}
             }
             ).then((response)=>{
                 if (response.data.msg === 'success'){
-                    alert('提交成功！\n我们将尽快通过acm@fudan.edu.cn联系您。\n如有任何问题或一周内未收到联系邮件，欢迎邮件联系我们！感谢您的投递:)')
-                    this.onReset()
+                    alert('已发送验证码，请查看邮箱。')
                 } else if (response.data.msg === 'fail'){
-                    alert('请重新提交。')
+                    alert('验证码发送错误。请再次尝试。')
                 }
             }).catch(function (error) {
                 if (error.response) {
@@ -160,23 +98,14 @@ export default class extends Component {
                 }
                 console.log(error.config)
             })
-            this.reloadPic()
-        } else {
-            this.reloadPic()
-            this.formRef.current.resetFields(['captchaCode'])
         }
-        console.log(values.captchaCode,' ', this.state.code)
-    }
-
-    onFinishFailed = (values) => {
-        this.reloadPic()
     }
 
     render() {
       return (
         <div className='main__container'>
             <div className='main__content'>
-                <Form {...layout} ref={this.formRef} name="control-ref" onFinish={this.onFinish} onFinishFailed={this.onFinishFailed} >
+                <Form {...layout} ref={this.formRef} name="control-ref" onFinish={this.onFinish}>
                     <Title>复旦大学程序设计竞赛队报名</Title>
                     <small>如有任何问题，欢迎邮件acm@fudan.edu.cn联系我们！</small>
                     <br/><br/>
@@ -286,23 +215,37 @@ export default class extends Component {
                             {
                                 required: true, message: '请填写邮箱',
                             },
+                            ({ getFieldValue }) => ({
+                              validator(rule, value) {
+                                mail=getFieldValue("email");
+                                  return Promise.resolve();
+                              }
+                            })
                         ]}
                     >
                         <Input />
                     </Form.Item>
-                    <Form.Item
-                        name="captchaCode"
-                        label="验证码"
-                        rules={[
-                            {
-                                required: true, message: '请输入验证码'
-                            },
-                        ]}
-                    >
-                        <Input 
-                            suffix={<canvas onClick={this.reloadPic} ref={this.canvas} width="90" height="28" />}
-                            placeholder="请输入验证码"
-                        />
+                    <Form.Item name="Captcha" label="验证码">
+                        <Row gutter={8}>
+                        <Col span={15}>
+                            <Form.Item
+                            name="captcha"
+                            noStyle
+                            rules={[
+                                {
+                                    required: true, message: '请填写验证码',
+                                },
+                            ]}
+                            >
+                            <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={6}>
+                            <Button type="button" onClick={this.SendCaptcha} >
+                                发送邮箱验证码
+                            </Button>
+                        </Col>
+                        </Row>
                     </Form.Item>
                     <Form.Item {...tailLayout}>
                         <Button type="primary" htmlType="submit" >
